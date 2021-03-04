8b60dbe414c2bed1096cdc2182fb304e56ba878e
arnoldo <Mauri@LAPTOP-0Q9ADC5L>
2021-03-04 11:25:42 -0800

    Calculo del perimetro

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar 3 variables a,b,c y asignarles valores estaticos 
+
+            int a = 10;
+            int b = 10;
+            int c = 7;
+
+            // Crear una variable de tipo int con la suma guardada de las variables 
+
+            int p = a + b + c;
+
+            //Mostrar en pantalla el valor de total "El perimetro es"
+            Console.WriteLine("El perimetro es:" + p);
+            Console.WriteLine("Toca alguna tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
